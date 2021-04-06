f137675099e6239857285693fbe30b897ef28447
sigma-sw <malek.horani4322@gmail.com>
2021-04-06 20:31:06 +0200

    INIT COMMIT

---
+++ crud_uppgiften/src/main/java/com/sigma_sw/crud_uppgiften/CrudUppgiftenApplication.java
@@ -0,0 +1,13 @@
+package com.sigma_sw.crud_uppgiften;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUppgiftenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUppgiftenApplication.class, args);
+	}
+
+}
---
+++ crud_uppgiften/src/test/java/com/sigma_sw/crud_uppgiften/CrudUppgiftenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma_sw.crud_uppgiften;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUppgiftenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
